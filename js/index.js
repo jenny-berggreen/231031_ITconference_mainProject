02d75d4cf1b1cc0cac78995d0e395f3b44760a23
Jenny Berggreen <143997461+jenny-berggreen@users.noreply.github.com>
2023-11-08 11:33:22 +0100

    Change navBar backgroundcolor when scrolling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
 
+const navBar = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+	if (window.scrollY > 1) {
+		displayNavColor();
+	} else {
+		hideNavColor();
+	}
+});
+
+function displayNavColor() {
+	navBar.style.backgroundColor = 'var(--color-primary)';
+}
+
+function hideNavColor() {
+  navBar.style.backgroundColor = 'transparent';
+}
+
 // ------------------------- accordion -------------------------
 
 const accordions = document.querySelectorAll('.accordion');
