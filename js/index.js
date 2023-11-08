aaa8a6998ebc79adf5b1169006759d562f486b36
Jenny Berggreen <143997461+jenny-berggreen@users.noreply.github.com>
2023-11-08 16:36:59 +0100

    Change slide when click on buttons

--- js/index.js
+++ js/index.js
@@ -9,13 +9,13 @@ window.addEventListener('scroll', function() {
 	}
 });
 
-function displayNavColor() {
+const displayNavColor = () => {
 	navBar.style.backgroundColor = 'var(--color-primary)';
-}
+};
 
-function hideNavColor() {
+const hideNavColor = () => {
   navBar.style.backgroundColor = 'transparent';
-}
+};
 
 // ------------------------- accordion -------------------------
 
@@ -24,9 +24,66 @@ const accordions = document.querySelectorAll('.accordion');
 accordions.forEach((accordion) => {
   const accordionButton = accordion.querySelector('.accordion__button');
 
-  function displayAccordionContent() {
+  const displayAccordionContent = () => {
     accordion.classList.toggle('accordion--expanded');
-  }
+  };
 
   accordionButton.addEventListener('click', displayAccordionContent);
 });
+
+// ------------------------- slideshow -------------------------
+
+const slideshow = document.querySelector('.slideshow');
+const slides = slideshow.querySelectorAll('.slideshow__slide');
+const controls = slideshow.querySelectorAll('.slideshow__control-button');
+
+let index = 0;
+const totalSlides = slides.length;
+const lastIndex = slides.length - 1;
+
+const goToPreviousSlide = () => {
+	if (index === 0) {
+		index = lastIndex;
+	} else {
+		index = index - 1;
+	}
+
+	// update classes
+	slides.forEach(slide => {
+		slide.classList.remove('slideshow__slide--visible');
+	});
+
+	slides[index].classList.add('slideshow__slide--visible');
+};
+
+const goToNextSlide = () => {
+	// increase index
+	if (index < lastIndex) {
+		index = index + 1;
+	} else {
+		index = 0;
+	}
+
+	// update classes
+	slides.forEach(slide => {
+		slide.classList.remove('slideshow__slide--visible');
+	});
+
+	slides[index].classList.add('slideshow__slide--visible');
+};
+
+const changeSlide = (event) => {
+	const button = event.currentTarget;
+
+	if (button.dataset.direction === 'previous') {
+		goToPreviousSlide();
+	}
+
+	if (button.dataset.direction === 'next') {
+		goToNextSlide();
+	}
+}
+
+controls.forEach(button => {
+	button.addEventListener('click', changeSlide);
+})
